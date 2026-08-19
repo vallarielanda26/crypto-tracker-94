41af48ac31c402df1fbf7a5f953dac65b961d468
Vallarie Landa <vallarieuxgo839@hotmail.com>
2026-08-19 09:17:17 +0200

    custom logger implementation for tracking

---
+++ logger.py
@@ -0,0 +1,26 @@
+import logging
+
+class Logger:
+    def __init__(self, name: str):
+        self.logger = logging.getLogger(name)
+        self.logger.setLevel(logging.DEBUG)
+        handler = logging.StreamHandler()
+        handler.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s'))
+        self.logger.addHandler(handler)
+
+    def debug(self, message: str):
+        self.logger.debug(message)
+
+    def info(self, message: str):
+        self.logger.info(message)
+
+    def warning(self, message: str):
+        self.logger.warning(message)
+
+    def error(self, message: str):
+        self.logger.error(message)
+
+    def critical(self, message: str):
+        self.logger.critical(message)
+
+logger = Logger('CryptoTracker')
