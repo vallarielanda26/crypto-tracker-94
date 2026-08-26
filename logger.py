bd13727ab27432f828cc32f5588479dbe25e9ac1
Vallarie Landa <vallarieuxgo839@hotmail.com>
2026-08-26 09:14:17 +0200

    logger setup with rotation

--- logger.py
+++ logger.py
@@ -1,26 +1,38 @@
 import logging
+from logging.handlers import RotatingFileHandler
+import os
 
-class Logger:
-    def __init__(self, name: str):
-        self.logger = logging.getLogger(name)
-        self.logger.setLevel(logging.DEBUG)
-        handler = logging.StreamHandler()
-        handler.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s'))
-        self.logger.addHandler(handler)
 
-    def debug(self, message: str):
-        self.logger.debug(message)
+def setup_crypto_logger(name: str = "crypto_tracker") -> logging.Logger:
+    log_dir = "logs"
+    if not os.path.exists(log_dir):
+        os.makedirs(log_dir)
 
-    def info(self, message: str):
-        self.logger.info(message)
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.DEBUG)
 
-    def warning(self, message: str):
-        self.logger.warning(message)
+    if not logger.handlers:
+        file_handler = RotatingFileHandler(
+            os.path.join(log_dir, "crypto_tracker.log"),
+            maxBytes=5 * 1024 * 1024,
+            backupCount=3,
+            encoding="utf-8",
+        )
+        file_handler.setLevel(logging.DEBUG)
 
-    def error(self, message: str):
-        self.logger.error(message)
+        console_handler = logging.StreamHandler()
+        console_handler.setLevel(logging.INFO)
 
-    def critical(self, message: str):
-        self.logger.critical(message)
+        formatter = logging.Formatter(
+            "%(asctime)s [%(levelname)s] %(name)s (%(threadName)s): %(message)s"
+        )
+        file_handler.setFormatter(formatter)
+        console_handler.setFormatter(formatter)
 
-logger = Logger('CryptoTracker')
+        logger.addHandler(file_handler)
+        logger.addHandler(console_handler)
+
+    return logger
+
+
+logger = setup_crypto_logger()
